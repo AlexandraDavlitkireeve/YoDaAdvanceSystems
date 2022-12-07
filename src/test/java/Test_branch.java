878b40f94f77d06b26ab107b86b4a2d8331413d4
Nikki Wilson <testn2005@gmail.com>
2022-12-07 10:11:38 -0500

    changes made for testing

--- src/test/java/Test_branch.java
+++ src/test/java/Test_branch.java
@@ -2,14 +2,13 @@ public class Test_branch {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("Burkhon");
-
         System.out.println("Nikki");
-
         System.out.println("Alexandra");
         System.out.println("Jose");
         System.out.println("Sugar");
         System.out.println("Pailin");
         System.out.println("Surapat");
+        System.out.println("git test");
     }
 
 
