df95bdc5b895def0060b66cc6346e7b2ba5fa84f
Burkhon <burkhon85@gmail.com>
2022-12-07 09:58:27 -0500

    third cmmit

--- src/test/java/Test_branch.java
+++ src/test/java/Test_branch.java
@@ -2,9 +2,7 @@ public class Test_branch {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("Burkhon");
-
         System.out.println("Nikki");
-
         System.out.println("Alexandra");
         System.out.println("Jose");
 
@@ -12,3 +10,4 @@ public class Test_branch {
 
 
 }
+git
