9c6a58b0de7d70c5924dd53399cefecf93b6526d
D Alexandra <alexandra.davlitkireeva@gmail.com>
2022-12-07 21:04:35 -0500

    edited some stuff

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,5 +1,7 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utils.BrowserUtils;
 
@@ -7,4 +9,12 @@ public class HomePage {
     public HomePage() {
         PageFactory.initElements(BrowserUtils.getDriver(), this);
     }
+    @FindBy(xpath = "//div[@class='info-box-one']/ul/li/i)[1]")
+    public WebElement streetAddress;
+
+    @FindBy(xpath = "//div[@class='info-box-one']/ul/li)[2]/strong")
+    public WebElement townAndState;
+
+    @FindBy(xpath = "//div[@class='info-box-one']/ul/li)[4]/strong")
+    public WebElement phoneNumber;
 }
