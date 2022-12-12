71562402c802e970c14157777c773bafe60a9ec3
Nikki Wilson <testn2005@gmail.com>
2022-12-12 16:50:52 -0500

    info on main page is updated

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -11,15 +11,6 @@ public class HomePage {
     public HomePage() {
         PageFactory.initElements(BrowserUtils.getDriver(), this);
     }
-    @FindBy(xpath = "//i[contains(text(), '10090 Main Street')]")
-    public WebElement streetAddress;
-
-    @FindBy(xpath = "//strong[contains(text(), 'Fairfax, VA, USA')]")
-    public WebElement townAndState;
-
-    @FindBy(xpath = "//strong[contains(text(), '+1 703-831-3217')]")
-    public WebElement phoneNumber;
-
 
     @FindBy (xpath = "//div[@class= 'top-right flex-box-three']")
     public WebElement navBtn;
