44c0992bd9da9850714c53052284f0d3ce7a13d2
D Alexandra <alexandra.davlitkireeva@gmail.com>
2022-12-18 10:31:19 -0500

    ASY-21 and 27

--- src/test/java/step_definitions/Api_steps.java
+++ src/test/java/step_definitions/Api_steps.java
@@ -1,25 +1,21 @@
 package step_definitions;
-
-import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
 import org.junit.Assert;
-import utils.BrowserUtils;
 
 import java.util.HashMap;
 import java.util.Map;
-
-import static io.restassured.http.ContentType.JSON;
+import static org.apache.http.HttpStatus.*;
 
 public class Api_steps {
     private static final String BASE_URL = "https://tla-school-api.herokuapp.com";
+    private static final String USERNAME = "user";
+    private static final String PASSWORD = "user123";
 
-    private static String token;
-    private static Response response;
+     Response response;
+
 
     Map<String, Object> variables;
 
@@ -27,57 +23,66 @@ public class Api_steps {
     String name;
     String duration;
 
-//    @Given("Verify a list of existing {string} course")
-//    public void verify_a_list_of_existing_course(String sdet) {
-//        RestAssured.baseURI = BASE_URL;
-//        String path = "/api/school/programs/sdetcourse";
-//
-//        response = RestAssured.given()
-//                .when()
-//                .get(path)
-//                .then()
-//                .statusCode(200)
-//                .log().all()
-//                .extract()
-//                .response();
-//        System.out.println(id);
-//        //Assert.assertEquals(id, response.jsonPath().getString("_id"));
-//        Assert.assertTrue(id.contains("_id"));
-//
-//        variables = new HashMap<>();
-//        variables.put("_id", id);
-//        variables.put("duration", duration);
-//        variables.put("name", name);
-//    }
 
-    @Given("Navigate to URL")
-    public void navigateToURL() {
-        BrowserUtils.getDriver().get(BASE_URL);
+    @Given("User gets Base URL")
+    public void userGetsBaseURL() {
+        RestAssured.baseURI = BASE_URL;
+    }
+    @When("User send GET request to the endpoint")
+    public void userSendGETRequestToTheEndpoint()  {
+        String path = "/api/school/programs/devcourse";
+        String path2 = "/api/school/programs/sdetcourse";
+//        String requestBody = "{\"_id\":\""+variables.get("_id")+"\",\"name\":\""+variables.get("name")+"\",\"duration\":\""+variables.get("duration")+"\",\"__v\":0}}}}}";
+//        System.out.println(requestBody);
+//        System.out.println(variables.get("_id"));
+//        System.out.println(variables.get("name"));
+//        System.out.println(variables.get("duration"));
+        response = RestAssured.given()
+                .when()
+                .get(path)
+                .then()
+                .statusCode(200)
+                .log().all()
+                .extract()
+                .response();
+        Assert.assertEquals("_id", "_id");
+
+        response = RestAssured.given()
+                .when()
+                .get(path2)
+                .then()
+                .statusCode(200)
+                .log().all()
+                .extract()
+                .response();
+
+        variables = new HashMap<>();
+        variables.put("_id", id);
+        variables.put("duration", duration);
+        variables.put("name", name);
     }
 
-    @When("I send GET request to the {string}")
-    public void iSendGETRequestToThe(String endPoint) {
-        RequestSpecification request = RestAssured.given().baseUri(endPoint);
-        response = request.when().get();
-        System.out.println(response);
-    }
-//    @When("I send GET request to the endpoint")
-//    public void iSendGETRequestToTheEndpoint() {
-//        RestAssured.baseURI = BASE_URL;
-//        String path = "/api/school/programs/sdetcourse";
-//        response = RestAssured.given()
-//                .when()
-//                .get(path)
-//                .then()
-//                .statusCode(200)
-//                .log().all()
-//                .extract()
-//                .response();
-//        System.out.println(variables.get(id));
-//    }
+    @Given("User sends request with username and password")
+    public void userSendsRequestWithUsernameAndPassword() {
+        String path = "/api/school/departments/gettoken";
 
-    @Then("the response should include a field called {string}")
-    public void theResponseShouldIncludeAFieldCalled(String course) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("username", "user");
+        map.put("password", "user123");
+
+        response = RestAssured.given()
+                .queryParams(map)
+                .when()
+                .get(path)
+                .prettyPeek()
+                .then()
+                .statusCode(SC_OK)
+                .extract()// Method that extracts the response JSON DATA
+                .body()// Body extracted as a JSON format
+                .jsonPath()// Navigate using jsonPath
+                .get("token"); // Get a value as a key token
+
+        Assert.assertEquals(SC_OK, response.statusCode());
 
     }
 
--- src/test/resources/features/api.feature
+++ src/test/resources/features/api.feature
@@ -1,13 +1,18 @@
 Feature: End to end test
 
   Background: URL
-    Given Navigate to URL
+    Given User gets Base URL
+
+  @ASY-27
+  Scenario: Bearer token retrieval
+  Given User sends request with username and password
+
 
   @ASY-21
   Scenario: GET existing courses
-    When I send GET request to the "endpoint"
-  Then the response should include a field called "id"
-  And  the response should include a field called "name"
-  And  the response should include a field called "duration"
+    When User send GET request to the endpoint
+#  Then the response should include a field called "id"
+#  And  the response should include a field called "duration"
+#  And  the response should include a field called "name"
 
 
