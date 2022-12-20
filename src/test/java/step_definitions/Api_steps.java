e22bf8de5c55511272f45ba2e1195a56c93e9cac
Nikki Wilson <testn2005@gmail.com>
2022-12-20 11:13:25 -0500

    added files for api testing

--- src/test/java/step_definitions/Api_steps.java
+++ src/test/java/step_definitions/Api_steps.java
@@ -1,45 +1,87 @@
 package step_definitions;
 
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import org.junit.Assert;
+import utils.ConfigReader;
+import java.util.List;
 
-import java.util.HashMap;
-import java.util.Map;
+import static io.restassured.RestAssured.given;
+import static org.apache.http.HttpStatus.SC_OK;
 
 public class Api_steps {
-    private static final String BASE_URL = "https://tla-school-api.herokuapp.com";
 
-    private static String token;
-    private static Response response;
+    private static final String path = "/api/school/programs/devcourse";
+    private static final String path2 = "/api/school/programs/sdetcourse";
+    private static final String path3 = "/api/school/resources/students";
 
-    Map<String, Object> variables;
+    Response response;
 
-    String id;
-    String name;
-    String duration;
+    @Given("User gets Base URL")
+    public void userGetsBaseURL() {
+        RestAssured.baseURI = ConfigReader.readProperty("BASE_URL");
+    }
 
-    @Given("Verify a list of existing {string} course")
-    public void verify_a_list_of_existing_course(String sdet) {
-        RestAssured.baseURI = BASE_URL;
-        String path = "/api/school/programs/sdetcourse";
-
-        response = RestAssured.given()
+    @When("User send GET request to the endpoint {string}")
+    public void userSendGETRequestToTheEndpoint(String endpoint) {
+        response = given()
                 .when()
                 .get(path)
                 .then()
-                .statusCode(200)
                 .log().all()
                 .extract()
                 .response();
-        System.out.println(id);
-        //Assert.assertEquals(id, response.jsonPath().getString("_id"));
-        Assert.assertTrue(id.contains("_id"));
+    }
+    @Then("User should get status code {int}")
+    public void userShouldGetStatusCode(int statusCode) {
+        Assert.assertEquals(statusCode, response.statusCode());
+    }
+    @Then("the response should include fields called {string}, {string} and {string}")
+    public void theResponseShouldIncludeFieldsCalledAnd(String field1, String field2, String field3) {
+        Assert.assertNotNull(response.jsonPath().getString("data." + field1 + "[0]"));
+        Assert.assertNotNull(response.jsonPath().getString("data." + field2 + "[0]"));
+        Assert.assertNotNull(response.jsonPath().getString("data." + field3 + "[0]"));
+    }
 
-        variables = new HashMap<>();
-        variables.put("_id", id);
-        variables.put("duration", duration);
-        variables.put("name", name);
+    @Then("the response should include following fields")
+    public void theResponseShouldIncludeFollowingFields(List<String> list) {
+        for (int i = 0; i < list.size(); i++) {
+            Assert.assertNotNull(response.jsonPath().getString("data." + list.get(i) + "[0]"));
+        }
+    }
+
+    @Then("User sends request with username and password")
+    public void userSendsRequestWithUsernameAndPassword() {
+        String path = "/api/school/departments/gettoken";
+
+        response = given()
+                .auth().preemptive().basic("user", "user123")
+                .when()
+                .get(path)
+                .prettyPeek()
+                .then()
+                .statusCode(SC_OK)
+                .extract()// Method that extracts the response JSON DATA
+                .response();
+
+        Assert.assertEquals(SC_OK, response.statusCode());
+    }
+    @Then("Create option to add new student to db using the following id parameter")
+    public void createOptionToAddNewStudentToDbUsingTheFollowingIdParameter(List list)
+    {
+
+        response = given()
+                .when()
+                .post(path)
+                .then().log().all()
+                .extract()
+                .response();
+
     }
 }
+
+
+
--- src/test/resources/features/api.feature
+++ src/test/resources/features/api.feature
@@ -1,6 +1,43 @@
+
 Feature: End to end test
 
-@ASY-21
-Scenario: GET existing courses
-  Given Verify a list of existing "sdet" course
+  Background: URL
+    Given User gets Base URL
+
+  @ASY-21
+  Scenario: GET existing courses
+    When User send GET request to the endpoint "/programs/devcourse"
+    Then User should get status code 200
+    When User send GET request to the endpoint "/programs/sdetcourse"
+    Then User should get status code 200
+#    Then the response should include fields called "_id", "name" and "duration"
+    Then the response should include following fields
+      | "_id"       |
+      | "name"     |
+      | "duration" |
+
+
+  @ASY-24
+  Scenario: Add new student using POST request
+    Then Create option to add new student to db using the following id parameter
+      | "batch name" |
+      | "firstname"  |
+      | "lastname"   |
+      | "email"      |
+
+
+#  @ASY-25
+#  Scenario: Performing PUT request for updating existing student
+#    Then I add new student to db with fields using end point "/api/school/resources/students"
+#    Then I update existing student with _id parameter using end point "/api/school/resources/students/"
+#    Then I should see an update existing student
+
+  @ASY-27
+  Scenario: Bearer token retrieval
+    When User adds basic auth with username "user" and password "user123"
+    And User send GET request to the endpoint "/departments/gettoken"
+    Then response should contain a token
+    Then response should contain a message "Invalid username or password"
+
+
 
